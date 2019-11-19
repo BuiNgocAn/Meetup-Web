58df08d3d2484f72a19c73be877eff67f895d199
An <anbn247@gmail.com>
2019-11-19 12:51:36 +0700

    update user model

--- server/models/UserModel.js
+++ server/models/UserModel.js
@@ -33,19 +33,19 @@ const UserSchema = mongoose.Schema({
 }, { timestamps: true });
 
 UserSchema.pre('save', async function preSave(next) {
-    const user = this;
-    if (!user.isModified('password')) return next();
-    try {
-        const hash = await bcrypt.hash(user.password, SALT_ROUNDS);
-        user.password = hash;
-        return next();
-    } catch (err) {
-        return next(err);
-    }
+  const user = this;
+  if (!user.isModified('password')) return next();
+  try {
+    const hash = await bcrypt.hash(user.password, SALT_ROUNDS);
+    user.password = hash;
+    return next();
+  } catch (err) {
+    return next(err);
+  }
 });
 
 UserSchema.methods.comparePassword = async function comparePassword(candidate) {
-    return bcrypt.compare(candidate, this.password);
+  return bcrypt.compare(candidate, this.password);
 };
 
 module.exports = mongoose.model('User', UserSchema);
